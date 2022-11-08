004a047afa4c3d7647c06fcf29bbe6a5c9f894b5
Adam Courington <adamcourington@gmail.com>
2022-11-08 15:18:48 -0600

    Added test to see if there are 52 cards in my deck

--- index_test.js
+++ index_test.js
@@ -1,9 +1,13 @@
 var expect = chai.expect;
 
-describe('MyFunctions', function () {
-  describe('#freshDeck', function () {
-    it('Should create array to hold suite and values together', function () {
-      var x = freshDeck();
-    });
+describe('createTestDeck', () => {
+  it('Does the deck contain 52 cards?', function (done) {
+    const deck = new Deck();
+    let testDeck = new Deck(deck.cards);
+    let testDeckLength = testDeck.cards.length;
+    console.log(testDeckLength);
+
+    expect(testDeckLength).to.eql(52);
+    done();
   });
 });
