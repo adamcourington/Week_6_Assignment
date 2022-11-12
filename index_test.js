8a5dc56cb695c3b1fe8413141cf7561a1272a090
Adam Courington <adamcourington@gmail.com>
2022-11-12 12:01:24 -0600

    Added 2nd Test

--- index_test.js
+++ index_test.js
@@ -10,4 +10,15 @@ describe('createTestDeck', () => {
     expect(testDeckLength).to.eql(52);
     done();
   });
+
+  it('Does player deck contain 26 cards?', function (done) {
+    const deck = new Deck();
+    deck.shuffle();
+    let testDeck = new Deck(deck.cards.length / 2);
+    let testDeckAmount = testDeck.cards;
+    console.log(testDeckAmount);
+
+    expect(testDeckAmount).to.eql(26);
+    done();
+  });
 });
